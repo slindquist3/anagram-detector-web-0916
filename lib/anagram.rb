9abfe44559c237a948cb42c2ec959ae873af6a46
slindquist3 <slindquist3@gmail.com>
2016-09-29 14:59:50 -0400

    found anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+
+  end
+
+  def match(possible_anagrams)
+    possible_anagrams.select {|single| @word.split("").sort! == single.split("").sort!}
+  end
+
+end
